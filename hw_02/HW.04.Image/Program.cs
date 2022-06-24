40eeb76168a25051998ed60280de2fde4ccae2dd
Viktor <vitalineage22@gmail.com>
2022-06-24 18:46:49 +0300

    feat: HW.04. complited

---
+++ hw_02/HW.04.Image/Program.cs
@@ -0,0 +1,12 @@
+﻿StreamReader textReader = new StreamReader(@"D:\Work\C#\image.txt", true);
+string textReaderResult = textReader.ReadToEnd();
+string[] arrayOfTextResult = textReaderResult.Split(' ');
+byte[] imageBytes = new byte[arrayOfTextResult.Length - 1];
+
+for (int i = 0; i < arrayOfTextResult.Length - 1; i++)
+{
+    byte binary = Convert.ToByte(arrayOfTextResult[i], 2);
+    imageBytes[i] = binary;
+}
+File.WriteAllBytes(@"D:\Work\C#\hw_dotnet\hw_02\HW.04.Image\image\image.png", imageBytes);
+textReader.Dispose();
