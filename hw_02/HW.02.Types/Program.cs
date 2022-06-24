9969551332f2f048171a4f2036cee1208b3c6bd1
Viktor <vitalineage22@gmail.com>
2022-06-24 17:40:44 +0300

    feat: HW.02.Types complited

--- hw_02/HW.02.Types/Program.cs
+++ hw_02/HW.02.Types/Program.cs
@@ -3,24 +3,68 @@ int val1 = 35;
 Int32 val2 = 45;
 Console.WriteLine(val1.GetType()); 
 Console.WriteLine(val2.GetType());
-//
+//Example 2
 byte @byte1 = 1;
 Byte @byte2 = 2;
 Console.WriteLine(byte1.GetType());
 Console.WriteLine(byte2.GetType());
-//
+//Example 3
 char sign1 = '-';
 Char sign2 = '+';
 Console.WriteLine(sign1.GetType());
 Console.WriteLine(sign2.GetType());
-//
+//Example 4
 bool isDone = true;
 Boolean isntDone = false;
 Console.WriteLine(isDone.GetType());
 Console.WriteLine(isntDone.GetType());
-//
+//Example 5
 sbyte bit1 = -5;
 SByte bit2 = 55;
 Console.WriteLine(bit1.GetType());
 Console.WriteLine(bit2.GetType());
-//
+//Example 6
+short sh1 = 5;
+Int16 sh2 = 55;
+Console.WriteLine(sh1.GetType());
+Console.WriteLine(sh2.GetType());
+//Example 7
+ushort ush1 = 1;
+UInt16 ush2 = 56;
+Console.WriteLine(ush1.GetType());
+Console.WriteLine(ush2.GetType());
+//Example 8
+long l1 = 465;
+Int64 l2 = 8465132;
+Console.WriteLine(l1.GetType());
+Console.WriteLine(l2.GetType());
+//Example 9
+ulong ul1 = 465;
+UInt64 ul2 = 8465132;
+Console.WriteLine(ul1.GetType());
+Console.WriteLine(ul2.GetType());
+//Example 10
+float f1 = 465;
+Single f2 = 1254;
+Console.WriteLine(f1.GetType());
+Console.WriteLine(f2.GetType());
+//Example 11
+double d1 = 1.7;
+Double d2 = 5.3724;
+Console.WriteLine(d1.GetType());
+Console.WriteLine(d2.GetType());
+//Example 12
+decimal dec1 = 465;
+Decimal dec2 = 123;
+Console.WriteLine(dec1.GetType());
+Console.WriteLine(dec2.GetType());
+//Example 13
+string str1 = "Hello";
+String str2 = "Bye";
+Console.WriteLine(str1.GetType());
+Console.WriteLine(str2.GetType());
+//Example 14
+object obj1 = 465;
+Object obj2 = "Nice one";
+Console.WriteLine(obj1.GetType());
+Console.WriteLine(obj2.GetType());
