cf87b45acb52e29b5a060f3ba11f2c42a539801c
Viktor <vitalineage22@gmail.com>
2022-06-24 17:27:46 +0300

    upd: projs location upd

--- hw_02/HW.02.Types/Program.cs
+++ hw_02/HW.02.Types/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Example 1
+int val1 = 35;
+Int32 val2 = 45;
+Console.WriteLine(val1.GetType()); 
+Console.WriteLine(val2.GetType());
+//
+byte @byte1 = 1;
+Byte @byte2 = 2;
+Console.WriteLine(byte1.GetType());
+Console.WriteLine(byte2.GetType());
+//
+char sign1 = '-';
+Char sign2 = '+';
+Console.WriteLine(sign1.GetType());
+Console.WriteLine(sign2.GetType());
+//
+bool isDone = true;
+Boolean isntDone = false;
+Console.WriteLine(isDone.GetType());
+Console.WriteLine(isntDone.GetType());
+//
+sbyte bit1 = -5;
+SByte bit2 = 55;
+Console.WriteLine(bit1.GetType());
+Console.WriteLine(bit2.GetType());
+//
