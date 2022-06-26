5438dbed7002d497454517e814aad257a83a52ad
Viktor <vitalineage22@gmail.com>
2022-06-26 20:15:57 +0300

    feat: HW03.Birthday compl

--- CSharp.Homeworks/HW03.Birthday/Program.cs
+++ CSharp.Homeworks/HW03.Birthday/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Please, enter the year of birth: ");
+int year = int.Parse(Console.ReadLine());
+Console.WriteLine("Please, enter the month of birth: ");
+int month = int.Parse(Console.ReadLine());
+Console.WriteLine("Please enter the current year: ");
+int yearToday = int.Parse(Console.ReadLine());
+Console.WriteLine("Please enter the current month by it's number: ");
+int monthToday = int.Parse(Console.ReadLine());
+Console.WriteLine($"Today is {yearToday}, {monthToday}");
+int monthOld = monthToday - month;
+if (monthOld <= 0)
+{
+    int yearOld = yearToday - (year + 1);
+    Console.WriteLine($"It's age: {yearOld}");
+}
+else
+{
+    int yearOld = yearToday - year;
+    Console.WriteLine($"It's age: {yearOld}");
+}
+
