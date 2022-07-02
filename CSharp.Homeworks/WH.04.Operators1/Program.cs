8cac9baea78935ba4ba4c0525ac191ce645c5bcc
Viktor <vitalineage22@gmail.com>
2022-07-02 16:08:16 +0300

    feat: WH.04.Operators1 complited

---
+++ CSharp.Homeworks/WH.04.Operators1/Program.cs
@@ -0,0 +1,22 @@
+﻿int num1, num2;
+
+void EnterData()
+{
+    Console.WriteLine("Please, enter number 1: ");
+    num1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Please, enter number 2: ");
+    num2 = int.Parse(Console.ReadLine());
+}
+
+try
+{
+    EnterData();
+}
+catch (System.FormatException ex)
+{
+    Console.WriteLine($"There is an exception {ex}, please enter correct data: ");
+    EnterData();
+}
+
+int sum = num1 + num2;
+Console.WriteLine($"The amount of {num1} and {num2} is: {sum}");
