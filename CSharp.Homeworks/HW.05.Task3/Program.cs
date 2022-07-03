ff2a483f8c94c725f304cec0b8de5daa63b7e05a
Viktor <vitalineage22@gmail.com>
2022-07-03 15:24:34 +0300

    feat: HW.05.Task3 completed

---
+++ CSharp.Homeworks/HW.05.Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] randArray = new int[5];
+Random rand = new Random();
+
+for (int i = 0; i < randArray.Length; i++)
+{
+    randArray[i] = rand.Next();
+}
+foreach (int i in randArray)
+{
+    Console.WriteLine($"Your array numbers: {i}");
+}
+
+Console.WriteLine("\n");
+Array.Reverse(randArray);
+
+foreach (int i in randArray)
+{
+    Console.WriteLine($"Your new array numbers order: {i}");
+}
