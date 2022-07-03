ff61d01c055d76f66bd15a1f23333f00b1ae09c7
Viktor <vitalineage22@gmail.com>
2022-07-03 15:52:14 +0300

    feat: optional task for Task3 added (with imgs)

--- CSharp.Homeworks/HW.05.Task3/Program.cs
+++ CSharp.Homeworks/HW.05.Task3/Program.cs
@@ -17,3 +17,31 @@ foreach (int i in randArray)
 {
     Console.WriteLine($"Your new array numbers order: {i}");
 }
+//Optional task
+/*Int64[] randArray2 = new Int64[100];
+
+System.Diagnostics.Stopwatch.StartNew().Start();
+
+for (int i = 0; i < randArray2.Length; i++)
+{
+    randArray2[i] = rand.Next();
+}
+foreach (int i in randArray2)
+{
+    Console.WriteLine($"Your array numbers: {i}");
+}
+
+Console.WriteLine("\n");
+/*Array.Reverse(randArray2);
+
+foreach (int i in randArray2)
+{
+    Console.WriteLine($"Your new array numbers order: {i}");
+}
+for (int i = randArray2.Length; i > 0; i--)
+{
+    Console.WriteLine($"Your array numbers now: {i}");
+}
+System.Diagnostics.Stopwatch.StartNew().Stop();
+Console.WriteLine(System.Diagnostics.Stopwatch.StartNew().Elapsed);
+*/
