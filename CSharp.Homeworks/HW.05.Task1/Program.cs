5e9bb0e92e6fd5b213cb8e0958fea08a03f5fdc6
Viktor <vitalineage22@gmail.com>
2022-07-03 14:59:40 +0300

    ref: empty lines added

--- CSharp.Homeworks/HW.05.Task1/Program.cs
+++ CSharp.Homeworks/HW.05.Task1/Program.cs
@@ -2,15 +2,18 @@
 int[] enteredNum = new int[5];
 int[] sumOfPrev = new int[5];
 Random rand = new Random();
+
 for (int i = 0; i < randNum.Length; i++)
 {
     randNum[i] = rand.Next();
 }
+
 for (int i = 0; i < enteredNum.Length; i++)
 {
     Console.WriteLine("Enter your numbers");
     enteredNum[i] = int.Parse(Console.ReadLine());
 }
+
 for (int i = 0; i < sumOfPrev.Length; i++)
 {
     sumOfPrev[0] = randNum[0] + enteredNum[0];
@@ -19,14 +22,17 @@ for (int i = 0; i < sumOfPrev.Length; i++)
     sumOfPrev[3] = randNum[3] + enteredNum[3];
     sumOfPrev[4] = randNum[4] + enteredNum[4];
 }
+
 foreach (int i in enteredNum)
 {
     Console.WriteLine($"Elements of entered array: {i}\n");
 }
+
 foreach(int i in randNum)
 {
     Console.WriteLine($"Elements of random array: {i}\n");
 }
+
 foreach(int i in sumOfPrev)
 {
     Console.WriteLine($"Elements of sum array: {i}");
