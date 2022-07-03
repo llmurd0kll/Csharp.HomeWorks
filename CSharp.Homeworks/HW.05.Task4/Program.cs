29fec893d78c7b2b6daaf55b83c50c8fb15e862c
Viktor <vitalineage22@gmail.com>
2022-07-03 15:59:19 +0300

    HW.05.Task4 completed

---
+++ CSharp.Homeworks/HW.05.Task4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Enter the number. It should be >0 :");
+int entNum = int.Parse(Console.ReadLine());
+int result = 0;
+for (int i = 1; i <= entNum; ++i)
+{
+    result += i;
+    
+}
+Console.WriteLine(result);
