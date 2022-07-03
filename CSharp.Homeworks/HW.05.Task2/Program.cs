c227135e7179b214904e39bf43f6ede20782ab75
Viktor <vitalineage22@gmail.com>
2022-07-03 15:01:20 +0300

    feat: HW.05.Task2 completed

---
+++ CSharp.Homeworks/HW.05.Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿int[] array1 = new int[10];
+
+for (int i = 0; i < array1.Length-1; i++)
+{
+    Console.WriteLine("Enter your numbers");
+    array1[i] = int.Parse(Console.ReadLine());
+}
+
+foreach (int i in array1)
+{
+    Console.WriteLine($"Your array numbers: {i}\n");
+}
+
+Console.WriteLine("Enter your position for value");
+int pos = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your number for array");
+int newPosForValue = int.Parse(Console.ReadLine());
+
+int newPos;
+
+for (int i = pos; i < array1.Length-1; i++)
+{
+    newPos = array1[i];
+    array1[i] = newPosForValue;
+    newPosForValue = newPos;
+}
+
+foreach(int i in array1)
+{
+    Console.WriteLine($"Your array numbers now: {i}");
+}
