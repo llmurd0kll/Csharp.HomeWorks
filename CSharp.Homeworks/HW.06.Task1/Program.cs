af3f2a02afd1cc09a9946e493ef1df528d6137b0
Viktor <vitalineage22@gmail.com>
2022-07-09 17:34:23 +0300

    feat: HW.06. task1 completed

---
+++ CSharp.Homeworks/HW.06.Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Enter your poem in one row. Use ';' to divide rows");
+string poem = Console.ReadLine();
+char[] separators = new char[] {';'};
+
+string[] splitPoem = poem.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+
+foreach (string splitedPoem in splitPoem)
+{
+    string replacedLetters = splitedPoem;
+    replacedLetters = replacedLetters.Replace("a", "o");   
+    Console.WriteLine($"New poem: {replacedLetters}");
+}
