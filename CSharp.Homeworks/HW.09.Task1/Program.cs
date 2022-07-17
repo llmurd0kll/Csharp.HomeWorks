40b73ce566bb4a0814eef2752ce1116d11a89941
Viktor <vitalineage22@gmail.com>
2022-07-17 18:09:21 +0300

    feat: task1 completed

---
+++ CSharp.Homeworks/HW.09.Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿using HW._09.Task1;
+
+Person person = new Person();
+person.SayHello();
+Student student = new Student();
+student.age = 21;
+student.Greet();
+Teacher teacher = new Teacher();
+teacher.age = 30;
+teacher.SayHello();
+teacher.Explain();
