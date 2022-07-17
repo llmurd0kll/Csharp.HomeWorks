072faca94fe324212b730a343a1e351ff7033f29
Viktor <vitalineage22@gmail.com>
2022-07-17 21:12:59 +0300

    feat: task 2 completed

---
+++ CSharp.Homeworks/HW.09.Task2/Program.cs
@@ -0,0 +1,9 @@
+﻿using HW._09.Task2;
+
+
+SmallApartment apartment = new SmallApartment();
+apartment.Area = 55;
+apartment.ShowData();
+Person person = new Person();
+person.name = "Sam";
+person.ShowData();
---
+++ CSharp.Homeworks/HW.09.Task2/SmallApartment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW._09.Task2
+{
+    internal class SmallApartment : House
+    {
+        public override int  Area 
+        {           
+            get { return base.Area; }
+            set { base.Area = value; }
+        }
+       //public int area = 50;
+        public override void ShowData()
+        {
+            Console.WriteLine($"I'm a smallappartment, my area is {Area} m2");
+        }
+    }
+}
