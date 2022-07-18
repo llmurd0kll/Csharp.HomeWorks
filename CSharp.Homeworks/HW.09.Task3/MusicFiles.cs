4ebcf95a4d9d8e8902e013bf4000fc63ba26de2c
Viktor <vitalineage22@gmail.com>
2022-07-18 18:51:22 +0300

    init: HW.09.Task3 added

---
+++ CSharp.Homeworks/HW.09.Task3/MusicFiles.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW._09.Task3
+{
+    internal class MusicFiles : Catalog
+    {
+        public string singer;
+        public double length;
+
+        public void Play()
+        {
+
+        }
+
+        public void RetrieveInformation()
+        {
+
+        }
+    }
+}
