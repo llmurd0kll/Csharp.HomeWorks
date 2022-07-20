e0bbbd65be807232bc6a94f314546576bc0ff159
Viktor <vitalineage22@gmail.com>
2022-07-20 14:30:36 +0300

    init: HW.09.Booking.Com added

---
+++ CSharp.Homeworks/HW.09.Booking.Com/Models/Hotel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW._09.Booking.Com.Models
+{
+    internal class Hotel
+    {
+        public string[] Name { get; set; }
+        public string Address { get; set; }
+        public int Rate { get; set; }
+
+        public void Search()
+        {
+
+        }
+
+        public void Book()
+        {
+            Console.WriteLine("Please enter the name of the hotel you would like to book in: ");
+            string userInput = Console.ReadLine();
+            userInput.ToCharArray();
+          
+            for (int i = 0; i<=Name.Length; i++)
+            {
+                
+                if (Name.Equals(userInput))
+                {
+
+                }
+            }
+        }
+
+        public void Purchase()
+        {
+
+        }
+    }
+}
