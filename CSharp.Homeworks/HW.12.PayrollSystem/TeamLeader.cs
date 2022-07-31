7143f1ee5a9eeadf10b8e060f9fdd50944b59a74
Viktor <vitalineage22@gmail.com>
2022-07-31 16:35:46 +0300

    init: HW.12 sln added

---
+++ CSharp.Homeworks/HW.12.PayrollSystem/TeamLeader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW._12.PayrollSystem
+{
+    internal class TeamLeader
+    {
+    }
+}
