49b97b59c44c1831883f5dc1775eb151dc7ccc3e
Viktor <vitalineage22@gmail.com>
2022-08-07 15:49:58 +0300

    feat: task1 completed (Singleton pattern implementation)

--- CSharp.Homeworks/HM.13.Task1/Program.cs
+++ CSharp.Homeworks/HM.13.Task1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PatternSingleton;
+
+var singleton = Singleton.GetInstance("Привет, мир!");
+var singleton2 = Singleton.GetInstance("Здравствуй, космос!");
+Console.WriteLine(singleton2.Data);
+Console.ReadLine();
+
+/*
+Как мы видим, при попытке создания нескольких экземпляров класса, 
+мы получаем один единственный уникальный экземпляр класса, с единой точкой входа и реализующий механизм поздней инициализации. 
+Таким образом, мы полностью добились желаемого результата. */
