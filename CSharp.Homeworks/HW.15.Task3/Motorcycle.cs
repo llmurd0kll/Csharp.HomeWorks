3f4ac9cc9680aa9b51815e76f0507b2a18ad81a7
Viktor <vitalineage22@gmail.com>
2022-09-01 13:14:59 +0300

    feat: new feat added

--- CSharp.Homeworks/HW.15.Task3/Motorcycle.cs
+++ CSharp.Homeworks/HW.15.Task3/Motorcycle.cs
@@ -8,5 +8,37 @@ namespace HW._15.Task3
 {
     internal class Motorcycle
     {
+        public string Name { get; set; }
+
+        public string Model { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        private int _vinNumber = 111;
+
+        private int _odometer;
+
+        public int Odometer { get => _odometer; set => _odometer = value; }
+
+        public Motorcycle()
+        {
+        }
+        public Motorcycle(string name)
+        {
+            Name = name;
+        }
+        public Motorcycle(DateTime created, string name)
+        {
+        }
+
+        public int GetVinNumber()
+        {
+            return _vinNumber;
+        }
+
+        public override string ToString()
+        {
+            return $"Motorcycle: {Name}, Model: {Model}, Odometer: {Odometer}, VinNumber: {GetVinNumber()}";
+        }
     }
 }
