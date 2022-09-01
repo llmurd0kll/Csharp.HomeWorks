9e1c13483056d104de2669d3ef4843ceb49bb099
Viktor <vitalineage22@gmail.com>
2022-09-01 11:31:07 +0300

    feat: HW.15.Task1 completed

---
+++ CSharp.Homeworks/HW.15.Task1/ExtensionMethods.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW._15.Task1
+{
+    static class ModifyExtension
+    {
+        public static string Modify(this string text) 
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (i.Equals(9))
+                {
+                    text = text.Remove(i);
+                }
+            }
+            return text + "...";
+        }
+
+        public static string Converting(this string converting)
+        {
+            string output = converting switch
+            {
+                var i when i == "TRUE" => 1.ToString(),
+                var i when i == "FALSE" => 0.ToString() ,
+                _ => throw new ErrorWithConvertingException("Something went wrong. Converting is impossible")
+            };
+
+            return output;
+        }
+    }
+}
---
+++ CSharp.Homeworks/HW.15.Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿using HW._15.Task1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string text = "Hey guys from Csharp course!";
+        Console.WriteLine(text.Modify());
+
+        string converting = "TRUE";
+        Console.WriteLine(converting.Converting());
+    }
+}
+
